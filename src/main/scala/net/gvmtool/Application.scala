f1bea190cdd73bf89630c759746c1055344c145b
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-08-24 17:54:25 +0100

    Adds component scan to application bootstrap.

--- src/main/scala/net/gvmtool/Application.scala
+++ src/main/scala/net/gvmtool/Application.scala
@@ -2,8 +2,10 @@ package net.gvmtool
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
 
 @EnableAutoConfiguration
+@ComponentScan(Array("net.gvmtool"))
 class Application
 
 object Application {
