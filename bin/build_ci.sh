bf06af5b9d86ab830feb6e9eefb64f35c4bfaed7
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-08-24 07:17:31 +0100

    Adds cloud foundry config to build.

--- bin/build_ci.sh
+++ bin/build_ci.sh
@@ -14,4 +14,4 @@ if [[ "$RELEASE" == "1.0.0-SNAPSHOT" ]]; then
 fi
 
 echo "Environment: $ENV, Release: $RELEASE"
-./gradlew -Penv="$ENV" clean check #cfPush
+./gradlew -Penv="$ENV" clean check cfPush
