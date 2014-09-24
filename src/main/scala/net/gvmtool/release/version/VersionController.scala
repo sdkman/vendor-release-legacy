fff8179a93422faa6d7e9738082844da4fd4394f
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-09-24 22:12:37 +0100

    Adds exception handling for DataAccessExceptions.

--- src/main/scala/net/gvmtool/release/version/VersionController.scala
+++ src/main/scala/net/gvmtool/release/version/VersionController.scala
@@ -1,8 +1,9 @@
 package net.gvmtool.release.version
 
 import net.gvmtool.release.request.VersionRequest
-import net.gvmtool.release.response.Created
+import net.gvmtool.release.response.{ServiceUnavailable, Created}
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.DataAccessException
 import org.springframework.web.bind.annotation.RequestMethod.POST
 import org.springframework.web.bind.annotation._
 
@@ -12,6 +13,9 @@ trait VersionController {
 
   @RequestMapping(value = Array("/release"), method = Array(POST))
   def publish(@RequestBody request: VersionRequest) = Created(repo.save(Version(request)))
+
+  @ExceptionHandler
+  def handle(e: DataAccessException) = ServiceUnavailable(e.getMessage)
 
 }
 
