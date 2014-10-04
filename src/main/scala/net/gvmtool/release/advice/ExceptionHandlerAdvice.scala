21f07347689d50e6978df8e43ef93c9ab37bc8de
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-10-04 21:59:11 +0100

    Tweaks to imports.

--- src/main/scala/net/gvmtool/release/advice/ExceptionHandlerAdvice.scala
+++ src/main/scala/net/gvmtool/release/advice/ExceptionHandlerAdvice.scala
@@ -4,7 +4,7 @@ import net.gvmtool.release.candidate.CandidateNotFoundException
 import net.gvmtool.release.version.VersionNotFoundException
 import net.gvmtool.status._
 import org.springframework.dao.DataAccessException
-import org.springframework.web.bind.annotation.{ControllerAdvice, ExceptionHandler}
+import org.springframework.web.bind.annotation._
 
 @ControllerAdvice
 class ExceptionHandlerAdvice {
