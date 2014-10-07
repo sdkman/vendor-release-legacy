ac5329fa827337eb26e3af0ef5772721702913a0
Marco Vermeulen <vermeulen.mp@gmail.com>
2014-10-07 06:28:12 +0100

    Use ValidCandidate in release controller.

--- src/main/scala/net/gvmtool/release/releases.scala
+++ src/main/scala/net/gvmtool/release/releases.scala
@@ -18,7 +18,7 @@ package net.gvmtool.release
 import javax.validation.Valid
 
 import net.gvmtool.release.request.ReleaseRequest
-import net.gvmtool.release.validate.ValidRequest
+import net.gvmtool.release.validate.{ValidCandidate, ValidRequest}
 import net.gvmtool.status.Created
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.validation.BindingResult
@@ -29,9 +29,9 @@ trait ReleaseController extends CandidatePersistence with VersionPersistence {
   @RequestMapping(value = Array("/release"), method = Array(POST))
   def publish(@Valid @RequestBody request: ReleaseRequest)(implicit binding: BindingResult) =
     ValidRequest {
-      Option(candidateRepo.findByCandidate(request.getCandidate)).map { c =>
+      ValidCandidate {
         Created(versionRepo.save(Version(request)))
-      }.getOrElse(throw CandidateNotFoundException(request.getCandidate))
+      }
     }
 }
 
